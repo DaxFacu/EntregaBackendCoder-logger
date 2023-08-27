791eb1e42d90b959dc8afdd70ecaa7c48ff43d56
DaxFacu <113784822+DaxFacu@users.noreply.github.com>
2023-08-26 23:04:02 -0300

    Archivos generales

---
+++ src/services/products.service.js
@@ -0,0 +1,66 @@
+import { productsMongo } from "../DAO/mongo/products.mongo.js";
+
+class ProductsService {
+  getAllProducts = async () => {
+    const products = await productsMongo.getAllProducts();
+    return products;
+  };
+
+  async createProduc(
+    title,
+    description,
+    code,
+    price,
+    status,
+    stock,
+    category,
+    thumbnail,
+    id
+  ) {
+    console.log(title);
+    const productCreated = await productsMongo.createProduct(
+      title,
+      description,
+      code,
+      price,
+      status,
+      stock,
+      category,
+      thumbnail,
+      id
+    );
+    return productCreated;
+  }
+
+  updateProduct = async (
+    id,
+    title,
+    description,
+    code,
+    price,
+    status,
+    stock,
+    category,
+    thumbnail
+  ) => {
+    const productUpdated = await ProductsMongo.updateProduct(
+      id,
+      title,
+      description,
+      code,
+      price,
+      status,
+      stock,
+      category,
+      thumbnail
+    );
+    return productUpdated;
+  };
+
+  deleteProduct = async (id) => {
+    const deleted = await ProductsMongo.deleteProduct(id);
+    return deleted;
+  };
+}
+
+export const productsService = new ProductsService();
